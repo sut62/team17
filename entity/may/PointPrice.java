f0262334919295b3aef48e9d27181a71c2f8d063
Paweenoot <may_tk189@hotmail.com>
2019-12-16 00:28:17 +0700

    close #63 - add sub Entity for Collect Point system

---
+++ entity/may/PointPrice.java
@@ -0,0 +1,22 @@
+package com.okta.springbootvue.entity.may;
+
+import lombok.*;
+import javax.persistence.*;
+import java.util.Collection;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name="POINT_PRICE")
+public class PointPrice {
+	@Id
+	@SequenceGenerator(name="POINT_PRICE_SEQ",sequenceName="POINT_PRICE_SEQ")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="POINT_PRICE_SEQ")
+	@Column(name="POINT_PRICE_ID",unique = true, nullable = true)
+	private @NonNull Long id;
+
+	private @NonNull Integer type;
+
+	@OneToMany(fetch = FetchType.EAGER)
+	private Collection<CollectPoint> collectpoint;
+}
