3d19f7752d4df97105591003ce601fda5b969b37
taorawee <taorawee@gmail.com>
2019-12-15 16:02:04 +0700

    close #28 - add all controller of Register system
    git commit -am close

---
+++ controller/ta/RegisterController.java
@@ -0,0 +1,64 @@
+package com.okta.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.okta.backend.entity.ta.*;
+import com.okta.backend.repository.*;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+public class RegisterController {
+
+    @Autowired
+    private final RegisterRepository registerRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+    @Autowired
+    private TitleNameRepository titlenameRepository;
+    @Autowired
+    private GenderRepository genderRepository;
+
+
+    RegisterController(RegisterRepository registerRepository) {
+        this.registerRepository = registerRepository;
+    }
+
+    @GetMapping("/register")
+    public Collection<Register> Registers() {
+        return registerRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+    @PostMapping("/register/{name}/{lname}/{address}/{tel}/{employee_id}/{titlename_id}/{gender_id}")
+    public Register newRegister(Register newRegister,
+    @PathVariable String name,
+    @PathVariable String lname,
+    @PathVariable String address,
+    @PathVariable Long tel,
+    @PathVariable long employee_id,
+    @PathVariable long titlename_id,
+    @PathVariable long gender_id) {
+ 
+    Employee employee = employeeRepository.findById(employee_id);
+    TitleName titlename = titlenameRepository.findById(titlename_id);
+    Gender gender = genderRepository.findById(gender_id);
+
+    newRegister.setEmployee(employee);
+    newRegister.setTitlename(titlename);
+    newRegister.setName(name);
+    newRegister.setLname(lname);
+    newRegister.setAddress(address);
+    newRegister.setTel(tel);
+    newRegister.setGender(gender);
+
+    return registerRepository.save(newRegister); //บันทึก Objcet ชื่อ Register
+    
+    }
+}
---
+++ controller/ta/TitleNameController.java
@@ -0,0 +1,37 @@
+package com.okta.backend.controller;
+
+import com.okta.backend.entity.ta.TitleName;
+import com.okta.backend.repository.TitleNameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+public class TitleNameController {
+
+    @Autowired
+    private final TitleNameRepository titlenameRepository;
+
+    public TitleNameController(TitleNameRepository titlenameRepository) {
+        this.titlenameRepository = titlenameRepository;
+    }
+
+    @GetMapping("/titlename")
+    public Collection<TitleName> TitleNames() {
+        return titlenameRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+    @GetMapping("/titlename/{id}")
+    public Optional<TitleName> TitleNames(@PathVariable Long id) {
+        Optional<TitleName> titlename = titlenameRepository.findById(id);
+        return titlename;
+    }
+
+}
