457f5f8d557362aff1ad30ecd9e818e9fcbe4415
PattaOil <ptn.220941@hotmail.com>
2020-01-06 19:58:47 +0700

    close #68 - add client folder

---
+++ client/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+  <v-app-bar
+      app
+      color="cyan darken-3"
+      dark
+    >
+        <span class="display-1 font-italic"> ADD EMPLOYEE </span>
+      <v-spacer></v-spacer>
+      <v-btn
+      dark
+      color = "red darken-1"
+      >
+        <span class="mr-2">log out</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content class="teal lighten-3">
+      <v-col></v-col>
+      <Employee/>
+    </v-content>
+ 
+  </v-app>
+</template>
+
+<script>
+import Employee from './components/Employee';
+
+export default {
+  name: 'App',
+
+  components: {
+    Employee,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
