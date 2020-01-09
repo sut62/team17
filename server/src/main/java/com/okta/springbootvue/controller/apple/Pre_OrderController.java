f271d9cacb4eb0faefaed470e9ec7604ced8a1d9
Sirirat Phonbulop <Tomatokamisama24@gmail.com>
2020-01-09 14:01:42 +0700

    close #73 Change Number to Quantity

--- server/src/main/java/com/okta/springbootvue/controller/apple/Pre_OrderController.java
+++ server/src/main/java/com/okta/springbootvue/controller/apple/Pre_OrderController.java
@@ -39,9 +39,9 @@ public class Pre_OrderController {
     @Autowired
     private Type_ProductRepository type_productRepository;
     @Autowired
-    private Title_NameRepository title_nameRepository;
+    private TitleNameRepository titlenameRepository;
     @Autowired
-    private NumberRepository numberRepository;
+    private QuantityRepository quantityRepository;
 
     Pre_OrderController(Pre_OrderRepository pre_orderRepository) {
         this.pre_orderRepository = pre_orderRepository;
@@ -52,31 +52,30 @@ public class Pre_OrderController {
         return pre_orderRepository.findAll().stream().collect(Collectors.toList());
     }
 
-    @PostMapping("/pre_order/{employeeID}/{title_nameID}/{cus_name}/{tel}/{brandID}/{type_productID}/{numberID}")
+    @PostMapping("/pre_order/{employeeID}/{titlenameID}/{cus_name}/{tel}/{brandID}/{type_productID}/{quantityID}")
     public Pre_Order newPreOrder (Pre_Order newPreOrder,
-
     @PathVariable long employeeID,
-    @PathVariable long title_nameID,
+    @PathVariable long titlenameID,
     @PathVariable String cus_name,
     @PathVariable String tel,
     @PathVariable long brandID,
     @PathVariable long type_productID,
-    @PathVariable long numberID) {
+    @PathVariable long quantityID) {
 
-    Title_Name title_name = title_nameRepository.findById(title_nameID);
+    TitleName titlename = titlenameRepository.findById(titlenameID);
     Brand brand = brandRepository.findById(brandID);
     Type_Product type_product = type_productRepository.findById(type_productID);
-    Number number = numberRepository.findById(numberID);
+    Quantity quantity = quantityRepository.findById(quantityID);
     Employee employee = employeeRepository.findById(employeeID);
     LocalDateTime now = LocalDateTime.now();
 
     newPreOrder.setEmployee(employee);
-    newPreOrder.setTitle_name(title_name);
+    newPreOrder.setTitlename(titlename);
     newPreOrder.setCus_name(cus_name);
     newPreOrder.setTel(tel);
     newPreOrder.setBrand(brand);
     newPreOrder.setType_product(type_product);
-    newPreOrder.setNumber(number);
+    newPreOrder.setQuantity(quantity);
     newPreOrder.setDate(now);
 
     return pre_orderRepository.save(newPreOrder);
--- server/src/main/java/com/okta/springbootvue/repository/apple/QuantityRepository.java
+++ server/src/main/java/com/okta/springbootvue/repository/apple/QuantityRepository.java
@@ -1,12 +1,12 @@
 package com.okta.springbootvue.repository.apple;
 
-import com.okta.springbootvue.entity.apple.Number;
+import com.okta.springbootvue.entity.apple.Quantity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public
-interface NumberRepository extends JpaRepository<Number, Long> {
-	Number findById(long id);
+interface QuantityRepository extends JpaRepository<Quantity, Long> {
+	Quantity findById(long id);
 }
