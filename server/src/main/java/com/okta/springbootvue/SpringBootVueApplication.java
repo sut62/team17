e1a0e00a68dab4bdd5ad70ba52f3f36b9938f7d3
taorawee <taorawee@gmail.com>
2020-01-10 18:14:28 +0700

    close #86 - edit SpringBootVueApplication

--- server/src/main/java/com/okta/springbootvue/SpringBootVueApplication.java
+++ server/src/main/java/com/okta/springbootvue/SpringBootVueApplication.java
@@ -2,10 +2,8 @@ package com.okta.springbootvue;
 
 import com.okta.springbootvue.entity.oil.*;
 import com.okta.springbootvue.repository.oil.*;
-import com.okta.springbootvue.entity.may.*;
-import com.okta.springbootvue.repository.may.*;
-import com.okta.springbootvue.entity.joy.*;
-import com.okta.springbootvue.repository.joy.*;
+import com.okta.springbootvue.entity.ta.*;
+import com.okta.springbootvue.repository.ta.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.ApplicationRunner;
@@ -17,12 +15,13 @@ import java.time.LocalDateTime;
 public class SpringBootVueApplication {  
   
     public static void main(String[] args) {  
-      SpringApplication.run(SpringBootVueApplication.class, args)  
+      SpringApplication.run(SpringBootVueApplication.class, args);  
     }  
 
     // Bootstrap some test data into the in-memory database
     @Bean  
-    ApplicationRunner init(PointPriceRepository pointPriceRepository, CollectPointRepository collectPointRepository, EmployeeRepository employeeRepository, GenderRepository genderRepository, TypeRepository typeRepository, VacancyRepository vacancyRepository,BrandRepository brandRepository, Type_ProductRepository type_productRepository) {  
+    ApplicationRunner init(EmployeeRepository employeeRepository, GenderRepository genderRepository, TypeRepository typeRepository, VacancyRepository vacancyRepository,
+    TitleNameRepository titlenameRepository) {  
         return args -> {  
             Stream.of("Male", "Female").forEach(gender -> {
                 Gender g = new Gender();
@@ -36,20 +35,14 @@ public class SpringBootVueApplication {
                 typeRepository.save(t);
             });
 
-            Stream.of(30, 50, 60).forEach(type -> {
-                PointPrice p = new PointPrice();
-                p.setType(type);
-                pointPriceRepository.save(p);
-            });
-
             Stream.of("Manager", "Employee").forEach(vacancy -> {
                 Vacancy v = new Vacancy();
                 v.setVacancy(vacancy);
                 vacancyRepository.save(v);
             });
 
-            Stream.of("oil").forEach(username -> {
-            Stream.of("2").forEach(password -> {
+            Stream.of("Oil Pattanan").forEach(username -> {
+            Stream.of("2222").forEach(password -> {
                 Employee e = new Employee();
                 LocalDateTime now = LocalDateTime.now();
                 e.setDate(now);
@@ -60,27 +53,18 @@ public class SpringBootVueApplication {
                 e.setVacancy(vacancyRepository.findById(1));
                 employeeRepository.save(e);
             });
-
             });
-           Stream.of("L’OREAL", "OLAY", "Maybelline", "AVON","Srichand").forEach(name -> {
-	Brand brand= new Brand(); 													
-	brand.setName(name); 												
-	brandRepository.save(brand); 												
-           });
 
-           Stream.of("Lipstick", "Eyeliner", "Cushion", "Blush","Mascara","Powder").forEach(name -> {
-	Type_Product type_product= new Type_Product(); 													
-	type_product.setName(name); 												
-	type_productRepository.save(type_product); 												
-           });
+            Stream.of("Mr.", "Miss.", "Professor.", "Dr.").forEach(title -> {
+				TitleName titlename = new TitleName(); // สร้าง Object Video
+				titlename.setTitle(title); // set ชื่อ (name) ให้ Object ชื่อ Video
+				titlenameRepository.save(titlename); // บันทึก Objcet ชื่อ Video
+			});
 
-            pointPriceRepository.findAll().forEach(System.out::println);
             genderRepository.findAll().forEach(System.out::println);
             typeRepository.findAll().forEach(System.out::println);
-            vacancyRepository.findAll().forEach(System.out::println);
-            brandRepository.findAll().forEach(System.out::println); 
-            type_productRepository.findAll().forEach(System.out::println);
-            
+            vacancyRepository.findAll().forEach(System.out::println); 
+            titlenameRepository.findAll().forEach(System.out::println);
         };  
     }  
  
