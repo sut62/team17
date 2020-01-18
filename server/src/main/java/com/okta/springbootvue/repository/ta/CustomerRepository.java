1485a7118b8554c6655a6589c96cc323191c9762
taorawee <taorawee@gmail.com>
2020-01-18 23:14:07 +0700

    close #104 - change system name repository to Customer

--- server/src/main/java/com/okta/springbootvue/repository/ta/CustomerRepository.java
+++ server/src/main/java/com/okta/springbootvue/repository/ta/CustomerRepository.java
@@ -1,12 +1,11 @@
 package com.okta.springbootvue.repository.ta;
 
-import com.okta.springbootvue.entity.ta.Register;
+import com.okta.springbootvue.entity.ta.Customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public
-interface RegisterRepository extends JpaRepository<Register, Long> {
-    Register findById(long id);
+interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
