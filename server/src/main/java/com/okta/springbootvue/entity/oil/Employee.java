1c1974ae394c4becd49ea3c238d91116a3bdd29e
PattaOil <ptn.220941@hotmail.com>
2020-01-21 23:45:55 +0700

    close #144 - edit entity for Integration EmployeeTests

--- server/src/main/java/com/okta/springbootvue/entity/oil/Employee.java
+++ server/src/main/java/com/okta/springbootvue/entity/oil/Employee.java
@@ -4,6 +4,8 @@ import lombok.*;
 import java.util.*;
 import java.time.LocalDateTime;
 import javax.persistence.*;
+import javax.validation.constraints.*;
+
 import com.okta.springbootvue.entity.oil.*;
 import com.okta.springbootvue.entity.joy.*;
 import com.okta.springbootvue.entity.may.CollectPoint;
@@ -11,23 +13,29 @@ import com.okta.springbootvue.entity.may.CollectPoint;
 @Data
 @Entity
 @NoArgsConstructor
-@Table(name="EMPLOYEE")
+@Table(name="EMPLOYEE",
+    uniqueConstraints = @UniqueConstraint(columnNames = {"USERNAME"})
+)
 public class Employee {
 
     @Id
     @SequenceGenerator(name="employee_seq",sequenceName="employee_seq")               
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="employee_seq")  
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="employee_seq") 
     @Column(name = "ID", unique = true, nullable = true)
-    private @NonNull Long id;
+    private Long id;
 
+    @NotNull
     @Column(name="DATE")
-    private @NonNull LocalDateTime date;
-
+    private LocalDateTime date;
+    
+    @NotNull
     @Column(name="USERNAME")
-    private @NonNull String username;
+    private String username;
 
+    @NotNull
+    @Size(min=8, max=16)
     @Column(name="PASSWORD")
-    private @NonNull String password;
+    private String password;
 
     @ManyToOne(fetch = FetchType.EAGER, targetEntity = Gender.class)
     @JoinColumn(name = "GENDER_ID", insertable = true)
@@ -42,7 +50,7 @@ public class Employee {
     private Vacancy vacancy;
 
     @OneToMany(fetch = FetchType.EAGER)
-    private Collection<Product> product;
+    private Collection<Add_Product> add_product;
 
     @OneToMany(fetch = FetchType.EAGER)
     private Collection<CollectPoint> collectpoint;
