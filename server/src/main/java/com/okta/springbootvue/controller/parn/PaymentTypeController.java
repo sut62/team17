f90a2e9c19dae8bf8c65f3ee6a52292e2db4a2d6
Paweenoot <may_tk189@hotmail.com>
2020-01-21 17:29:10 +0700

    close #127 - edit Controller For Payment System

--- server/src/main/java/com/okta/springbootvue/controller/parn/PaymentTypeController.java
+++ server/src/main/java/com/okta/springbootvue/controller/parn/PaymentTypeController.java
@@ -12,7 +12,7 @@ import com.okta.springbootvue.repository.parn.*;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class PaymentTypeController {
 
