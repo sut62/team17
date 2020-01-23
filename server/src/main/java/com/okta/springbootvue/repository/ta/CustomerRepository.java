4574af5eb38244ffbbc1b29c3a5cbdd716431a09
taorawee <taorawee@gmail.com>
2020-01-23 23:10:54 +0700

    close #116 - edit repository of Customer System

--- server/src/main/java/com/okta/springbootvue/repository/ta/CustomerRepository.java
+++ server/src/main/java/com/okta/springbootvue/repository/ta/CustomerRepository.java
@@ -1,13 +1,15 @@
 package com.okta.springbootvue.repository.ta;
 
 import com.okta.springbootvue.entity.ta.Customer;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.Collection;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public
 interface CustomerRepository extends JpaRepository<Customer, Long> {
     Customer findById(long id);
-    Customer findByTel(String tel);
+    Collection<Customer> findByTel(String tel);
 }
